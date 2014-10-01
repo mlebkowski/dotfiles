eb1b9f6ba3fd58acc0da0e9b190c46a39b7c5dc0
Maciej Łebkowski <m.lebkowski@gmail.com>
2014-10-01 13:47:41 +0200

    add pip and npm packages

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,3 @@
-set PATH /opt/local/bin /opt/local/sbin ~/bin ~/.composer/vendor/bin $PATH
+set PATH /opt/local/bin /opt/local/sbin ~/bin ~/.composer/vendor/bin ~/.dotfiles/node_modules/.bin $PATH
 set EDITOR nano
 set CDPATH . /Volumes/Dev
