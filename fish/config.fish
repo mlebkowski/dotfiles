e385db85698ef04a2ebf35b8e74d625e14631c0d
Maciej Łebkowski <m.lebkowski@gmail.com>
2016-02-06 00:26:55 +0100

    customs for voyager

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,14 @@
 set PATH /usr/local/php5/bin /opt/local/bin /opt/local/sbin ~/bin ~/.composer/vendor/bin ~/.dotfiles/node_modules/.bin ~/.gem/ruby/2.0.0/bin $PATH
 set EDITOR nano
-set CDPATH . /Volumes/Dev
+
+if test -d /Volumes/Dev;
+	set CDPATH . /Volumes/Dev
+end
+
 
 . ~/.config/fish/autoenv.fish
 _autoenvfish # check for starting directory
 
+for name in conf.d/*.fish;
+	. $name
+end
