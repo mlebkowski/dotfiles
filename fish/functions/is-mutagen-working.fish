bfb9b2aa615dbbf298ab6dff5ae2e44a05362370
Maciej Łebkowski <m.lebkowski@gmail.com>
2022-04-07 12:52:34 +0200

    rely on MUTAGEN_IDENTIFIER instead of directory to show mutagen status

--- fish/functions/is-mutagen-working.fish
+++ fish/functions/is-mutagen-working.fish
@@ -1,18 +1,4 @@
-# Defined in /var/folders/8n/4fbjz7dj23jc3g_9y274kk6h0000gn/T//fish.OBb4jd/is-mutagen-working.fish @ line 2
+# Defined in /var/folders/8n/4fbjz7dj23jc3g_9y274kk6h0000gn/T//fish.Rv8J7Z/is-mutagen-working.fish @ line 2
 function is-mutagen-working
-	set directory (pwd)
-
-    while [ $directory != "/" ]; 
-        if [ -f "$directory/docker-compose.yml" ];
-            break;
-        end
-
-        set directory (dirname "$directory")
-    end
-
-    if [ $directory = "/" ];
-        return 0
-    end
-
-    mutagen-status "$directory" 2>/dev/null
+  mutagen-status 2>/dev/null
 end
--- fish/functions/mutagen-status.fish
+++ fish/functions/mutagen-status.fish
@@ -1,29 +1,19 @@
-# Defined in /var/folders/8n/4fbjz7dj23jc3g_9y274kk6h0000gn/T//fish.2vq3Rk/mutagen-status.fish @ line 2
+# Defined in /var/folders/8n/4fbjz7dj23jc3g_9y274kk6h0000gn/T//fish.DgeZV4/mutagen-status.fish @ line 2
 function mutagen-status
-	set directory $argv[1]
+  if [ -z "$MUTAGEN_IDENTIFIER" ]; 
+    return 0;
+  end
 
-    set tmp (mktemp -d -t (echo $directory | md5))
-    set current (pwd)
-    set code 0
-
-    cd $tmp
-
-    mutagen list | split -p '----------'
-    
-    set status_file (grep -l "URL: $directory" "$tmp"/x?? | head -1)
-
-    if [ -f "$status_file" ]; and not grep -q "Status: Watching for changes" < "$status_file";
-        grep "Status" < "$status_file" >&2
-        if grep "Last error" < "$status_file" >&2; 
-            set code 2
+  set mstatus (mutagen sync list "$MUTAGEN_IDENTIFIER" 2>/dev/null)
+      
+  if [ -n "$mstatus" ]; and not printf "%s\n" "$mstatus" | grep -q "Status: Watching for changes";
+        printf "%s\n" "$mstatus" | grep "Status" >&2
+        if printf "%s\n" "$mstatus" | grep "Last error" >&2; 
+            return 2
         else 
-            set code 1
+            return 1
         end
     end
 
-    rm "$tmp"/x??
-    rmdir "$tmp"
-    cd "$current"
-
-    return $code
+    return 0
 end
