333daeaa681d22a64b1662aea709b5ad790c4732
Maciej Łebkowski <m.lebkowski@gmail.com>
2022-11-28 09:24:00 +0100

    new homebrew path

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set PATH /usr/local/php5/bin /opt/local/bin /opt/local/sbin ~/bin ~/.composer/vendor/bin ~/.dotfiles/node_modules/.bin ~/.gem/ruby/2.0.0/bin $PATH
+set PATH /opt/homebrew/bin ~/bin ~/.composer/vendor/bin ~/.dotfiles/node_modules/.bin ~/.gem/ruby/2.0.0/bin $PATH
 set EDITOR nano
 
 if test -d /Volumes/Dev;
