2636543949a8f9cf3a9023059dae529f2dc99a29
Maciej Łebkowski <m.lebkowski@gmail.com>
2023-06-12 22:58:33 +0200

    set up n paths

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set PATH /opt/homebrew/bin ~/bin ~/.composer/vendor/bin ~/.dotfiles/node_modules/.bin ~/.gem/ruby/2.0.0/bin $PATH
+set PATH /opt/homebrew/bin ~/bin ~/.composer/vendor/bin ~/.dotfiles/node_modules/.bin ~/.n/bin $PATH
 set EDITOR nano
 
 if test -d /Volumes/Dev;
@@ -15,5 +15,7 @@ end
 
 # set -e LC_ALL
 export LC_ALL=en_US.UTF-8
+
+export N_PREFIX=$HOME/.n
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
