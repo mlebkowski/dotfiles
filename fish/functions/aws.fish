8b9397e88864efef5139fed7bc428863f9719dbd
Maciej Łebkowski <m.lebkowski@gmail.com>
2024-04-06 11:54:02 +0200

    drop the aws cli in docker alias

--- fish/functions/aws.fish
+++
@@ -1,4 +0,0 @@
-function aws
-	set -l AWS_DEFAULT_REGION (if [ "$argv[1]" = "opsworks" ]; echo us-east-1; else; echo eu-west-1; end)
-	docker run --rm -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION anigeo/awscli $argv
-end
