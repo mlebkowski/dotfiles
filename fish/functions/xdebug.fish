acdc8cd0aabbbdeb0fd19aacf18869efc8db68c9
Maciej Łebkowski <m.lebkowski@gmail.com>
2024-05-22 20:38:52 +0200

    make the xdebug function search for the extension a bit

--- fish/functions/xdebug.fish
+++ fish/functions/xdebug.fish
@@ -1,3 +1,16 @@
 function xdebug
-	php -dzend_extension=xdebug.so -dxdebug.remote_autostart=On $argv
+  set --local xdebug_path xdebug.so
+  if ! php -dzend_extension=xdebug.so -m 2>/dev/null | grep -q '^xdebug$' ; 
+    set --local php_version (php -r 'echo PHP_MAJOR_VERSION, ".", PHP_MINOR_VERSION;')
+    set xdebug_path /opt/homebrew/opt/xdebug@$php_version/xdebug.so
+
+    if [ ! -f "$xdebug_path" ];
+      begin
+        echo "Couldn’t find xdebug.so in the default location"
+        echo "Tried $xdebug_path but it is not there either"
+      end >&2
+      return 1
+    end
+  end
+  php -dzend_extension="$xdebug_path" -dxdebug.mode=debug -dxdebug.start_with_request=yes $argv
 end
