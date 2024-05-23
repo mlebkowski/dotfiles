ec1502c8d5521626b344d094a1b707a56f73b3cb
Maciej Łebkowski <m.lebkowski@gmail.com>
2024-05-23 17:28:25 +0200

    split the autoenv stuff into a separate script

--- fish/autoenv.fish
+++ fish/autoenv.fish
@@ -1,83 +1,78 @@
-# AutoenvFish
-# Directory-based environments, inspired by Kenneth Reitz's autoenv
-
-# allow overriding of the default autoenvfish file name
-if not set -q AUTOENVFISH_FILE
-  set -g AUTOENVFISH_FILE ".env"
-end
-
-# Automatic env loading when the current working dir changes
-# inspired by virtualfish (https://github.com/adambrenecki/virtualfish)
 function _autoenvfish --on-variable PWD
-  if status --is-command-substitution # doesn't work with 'or', inexplicably
-    return
-  end
-
-	# find an autoenv file, checking up the directory tree until we find
-	# such a file (or nothing)
-  set -l envfishdir $PWD
-  while test ! "$envfishdir" = "" -a ! -d "$envfishdir/$AUTOENVFISH_FILE"
-    # this strips the last path component from the path.
-    set envfishdir (echo "$envfishdir" | sed 's|/[^/]*$||')
-  end
-  set -l envfishpath "$envfishdir/$AUTOENVFISH_FILE"
-  if [ $envfishpath != "$AUTOENVFISH" ]
-    # New path is different...
-    if [ -f $envfishpath -o -d $envfishpath ]
-      # congratulations, it's a file!
-      # variable change triggers loading of the fishenv file
-      set -g AUTOENVFISH $envfishpath
-    else
-      # file doesn't exist, so null out $AUTOENVFISH
-      set -e AUTOENVFISH
+    if status --is-command-substitution # doesn't work with 'or', inexplicably
+        return
     end
-  end
+
+    set -l current_bucket (nassau-env detect $PWD | md5)
+
+    if [ "$AUTOENVFISH_BUCKET" != "$current_bucket" ]
+        set -g AUTOENVFISH_BUCKET $current_bucket
+    end
 end
 
-# Triggered upon change of $AUTOENVFISH, source it if the file exists
-function _source_envfish --on-variable AUTOENVFISH
-  # fish bug: receiving erase from other shells :o
-  if [ $argv[2] = "ERASE" -a -n "$AUTOENVFISH" ];
-    return
-  end
-
-  while test (count $AUTOENVFISH_RESTORE) -gt 1;
-    set -gx $AUTOENVFISH_RESTORE[1..2]
-
-    if test (count $AUTOENVFISH_RESTORE) -gt 3;
-      set AUTOENVFISH_RESTORE $AUTOENVFISH_RESTORE[3..(count $AUTOENVFISH_RESTORE)]
-    else
-      set -e AUTOENVFISH_RESTORE
+function _autoenvfish_log
+    if status --is-interactive
+        gum log --prefix AUTOENVFISH $argv >&2
     end
-  end
+end
 
-  for name in $AUTOENVFISH_ERASE;
-    set -e $name
-  end
+# Triggered upon change of $AUTOENVFISH_BUCKET, source it if the file exists
+function _source_envfish --on-variable AUTOENVFISH_BUCKET
+    # fish bug: receiving erase from other shells :o
+    if [ $argv[2] = "ERASE" -a -n "$AUTOENVFISH_BUCKET" ];
+        return
+    end
 
-  if [ -d "$AUTOENVFISH" ]
-    set -l variables (ls -1 $AUTOENVFISH)
-    if [ (count $variables) -gt 0 ]
-      if status --is-interactive;
-        echo "loading $AUTOENVFISH ($variables)" >&2
-      end
+    set -l variables (nassau-env get "$PWD")
+    set -l names (echo $variables | jq -r 'keys|.[]')
 
-      for env in $variables;
-        if begin not contains $env $AUTOENVFISH_RESTORE; and set -q $env; end;
-          set -g AUTOENVFISH_RESTORE $AUTOENVFISH_RESTORE $env $$env
-        else if not contains $env $AUTOENVFISH_ERASE;
-          set -g AUTOENVFISH_ERASE $AUTOENVFISH_ERASE $env
+    # AUTOENVFISH_RESTORE = ['var1', 'oldvalue', 'var2', 'oldvalue', ....]
+    set -l restored_names
+    while test (count $AUTOENVFISH_RESTORE) -gt 1;
+        set -gx $AUTOENVFISH_RESTORE[1..2]
+
+        if not contains $AUTOENVFISH_RESTORE[1] $names;
+            set restored_names $restored_names $AUTOENVFISH_RESTORE[1]
         end
 
-        set -gx $env "$(cat $AUTOENVFISH/$env)"
-      end
+        if test (count $AUTOENVFISH_RESTORE) -gt 3;
+          set AUTOENVFISH_RESTORE $AUTOENVFISH_RESTORE[3..(count $AUTOENVFISH_RESTORE)]
+        else
+          set -e AUTOENVFISH_RESTORE
+        end
     end
-  else if [ -f "$AUTOENVFISH" ]
-    if status --is-interactive
-      echo "loading $AUTOENVFISH" >&2
+    if [ (count $restored_names) -gt 0 ]
+        _autoenvfish_log "restoring $restored_names"
     end
 
-    . $AUTOENVFISH
-  end
+    set -l erased_names
+    for name in $AUTOENVFISH_ERASE;
+        set -e $AUTOENVFISH_ERASE
+
+        if not contains $name $names;
+            set erased_names $erased_names $name
+        end
+    end
+    set -e AUTOENVFISH_ERASE
+    if [ (count $erased_names) -gt 0 ];
+        _autoenvfish_log "erasing $erased_names"
+    end;
+
+    set -l variables (nassau-env get "$PWD")
+    set -l names (echo $variables | jq -r 'keys|.[]')
+    if [ (count $names) -gt 0 ]
+        _autoenvfish_log "loading $names"
+
+        for name in $names;
+            set -l value "$(echo $variables | jq --arg key "$name" -r '.[$key]')"
+            if begin not contains $name $AUTOENVFISH_RESTORE; and set -q $name; end;
+                set -g AUTOENVFISH_RESTORE $AUTOENVFISH_RESTORE $name "$$name"
+            else if not contains $name $AUTOENVFISH_ERASE;
+                set -g AUTOENVFISH_ERASE $AUTOENVFISH_ERASE $name
+            end
+
+            set -gx $name "$value"
+        end
+    end
 end
 
