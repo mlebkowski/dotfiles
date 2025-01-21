9abae5e038eb2501cb3023d3cc3550ac418f7242
Maciej Łebkowski <m.lebkowski@gmail.com>
2025-01-21 14:17:45 +0100

    some brew package descriptions

--- fish/functions/brew-install-packages.fish
+++ fish/functions/brew-install-packages.fish
@@ -1,4 +1,4 @@
 function brew-install-packages 
   type brew >/dev/null 2>/dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  brew install (cat ~/.dotfiles/brew/packages)
+  brew install (cut -f1 -d" " ~/.dotfiles/brew/packages)
 end
